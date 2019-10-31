2a75dde551f9327fb26653efcb0f5b152e3a1c45
farahroslend <farahroslend@gmail.com>
2019-10-31 14:24:20 +0000

    options random scrambler, French word, navigator done

--- programming.js
+++ programming.js
@@ -71,14 +71,6 @@ function click_updatesCounterLEFT() {
 }
 
 
-// Updating the flashcards content
-function click_updatesFlashcardFront() {
- var myArray = ["niveau", 2, "banana", "apple", 5, 6, 7, 8, 9, 10];
- let currentCounter = sessionStorage.getItem("counter") || 1;
- var indexForFlashcardFront = currentCounter - 1;
- document.getElementById("FlashcardFront").innerHTML = myArray[indexForFlashcardFront];
-}
-
 //Flipping the flashcards on y-axis
 
 /*var ax = 'y';
@@ -102,3 +94,74 @@ $(document).ready(function() {
  });
  
 });*/
+
+
+//JS codes for quiz
+var myArrayBack = ["level", "speed", "sample", "target", "cost", "breakdown", "fire", "signal", "off spec", "flowrate", "heavy (distillation)", "light (distillation)", "valve", "safety valve", "tube side", "shell", "rate", "bed", "residence time", "salt"];
+var correctOptionPlacer = Math.round(Math.random());
+if (correctOptionPlacer = 1) {
+ document.getElementById("optionA").innerHTML = myArrayBack[currentCounter];
+ var wrongoptionIndex = (20 + 1) - currentCounter;
+ document.getElementById("optionB").innerHTML = myArrayBack[wrongoptionIndex];
+}
+else {
+ document.getElementById("optionB").innerHTML = myArrayBack[currentCounter];
+ var wrongoptionIndex = (20 + 1) - currentCounter;
+ document.getElementById("optionA").innerHTML = myArrayBack[wrongoptionIndex];
+}
+
+//the real codes for Quiz
+
+function click_updatesCounter() {
+ // set currentCounter variable to the contents of "counter" or 1 if "counter" doesn't exist yet
+ let currentCounter = sessionStorage.getItem("counter") || 1;
+ // increment currentCounter and store it in sessionStorage with the name "counter"
+ if (currentCounter == 20) {
+  //console.log("Do nothing");
+ }
+ else {
+  sessionStorage.setItem("counter", ++currentCounter);
+ }
+ var res = currentCounter + " " + "out of" + " " + 20;
+ document.getElementById("demo").innerHTML = res;
+
+
+ var myArrayFront = ["niveau", "vitesse", "echantillon", "cible", "coût", "panne", "feu", "consigne", "hors spec", "débit", "lourd", "légère", "vanne", "soupape", "tube côté", "calandre", "taux", "lit", "temps de résidence", "sel"];
+ document.getElementById("FlashcardFront").innerHTML = myArrayFront[currentCounter - 1];
+
+
+ //Updating content for optionA & optionB displays
+ var myArrayBack = ["level", "speed", "sample", "target", "cost", "breakdown", "fire", "signal", "off spec", "flowrate", "heavy (distillation)", "light (distillation)", "valve", "safety valve", "tube side", "shell", "rate", "bed", "residence time", "salt"];
+ var correctOptionPlacer = Math.round(Math.random());
+
+ var symmetryMiddle = 20 / 2;
+
+ if (currentCounter == symmetryMiddle) {
+  var correctOptionPlacer = Math.round(Math.random());
+  if (correctOptionPlacer == 1) {
+   document.getElementById("optionA").innerHTML = myArrayBack[currentCounter - 1];
+   var wrongoptionIndex = ((20 - 1) + 1) - currentCounter; //remember JS starts indexing with 0, not 1 like Matlab
+   document.getElementById("optionB").innerHTML = myArrayBack[0];
+  }
+  else {
+   document.getElementById("optionB").innerHTML = myArrayBack[currentCounter - 1];
+   var wrongoptionIndex = ((20 - 1) + 1) - currentCounter;
+   document.getElementById("optionA").innerHTML = myArrayBack[0];
+  }
+ }
+
+ else {
+
+  if (correctOptionPlacer == 1) {
+   document.getElementById("optionA").innerHTML = myArrayBack[currentCounter - 1];
+   var wrongoptionIndex = ((20 - 1) + 1) - currentCounter; //remember JS starts indexing with 0, not 1 like Matlab
+   document.getElementById("optionB").innerHTML = myArrayBack[wrongoptionIndex];
+  }
+  else {
+   document.getElementById("optionB").innerHTML = myArrayBack[currentCounter - 1];
+   var wrongoptionIndex = ((20 - 1) + 1) - currentCounter;
+   document.getElementById("optionA").innerHTML = myArrayBack[wrongoptionIndex];
+  }
+
+ }
+}
